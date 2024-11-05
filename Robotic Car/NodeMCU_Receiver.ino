ef33ca8ad351774c761f5577808bfac61768792f
Adhin V S <reachadhinvs@gmail.com>
2024-11-05 14:43:46 +0530

    Update NodeMCU_Receiver.ino

--- Robotic Car/NodeMCU_Receiver.ino
+++ Robotic Car/NodeMCU_Receiver.ino
@@ -1,170 +1,121 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 
-#define FORWARD 1
-#define BACKWARD 2
-#define LEFT 3
-#define RIGHT 4
-#define STOP 0
+// Right motor pins
+const int IN1 = 4;    // D2 - GPIO4
+const int IN2 = 0;    // D3 - GPIO0
+// Left motor pins 
+const int IN3 = 14;   // D5 - GPIO14
+const int IN4 = 12;   // D6 - GPIO12
 
-// Motor pins for L298N
-struct MOTOR_PINS
-{
-  int pinIN1;
-  int pinIN2;
-  int pinEn;
-};
-
-// Define motor pins (right motor and left motor)
-std::vector<MOTOR_PINS> motorPins = 
-{
-  {4, 0, 5},    // Right motor - IN1, IN2, ENA
-  {14, 12, 13}, // Left motor  - IN3, IN4, ENB
-};
-
-#define RIGHT_MOTOR 0
-#define LEFT_MOTOR 1
-
-#define MAX_MOTOR_SPEED 255
-
-#define SIGNAL_TIMEOUT 1000  // Signal timeout in milliseconds
 unsigned long lastRecvTime = 0;
+const int TIMEOUT = 1000;  // 1 second timeout
 
-struct PacketData
-{
-  byte xAxisValue;
-  byte yAxisValue;
-  byte zAxisValue;
+struct PacketData {
+  byte xAxis;
+  byte yAxis;
+  byte zAxis;
 };
-PacketData receiverData;
+PacketData data;
 
-// Callback function when data is received
-void OnDataRecv(uint8_t *mac_addr, uint8_t *incomingData, uint8_t len) 
-{
-  if (len == 0)
-  {
-    return;
-  }
-  memcpy(&receiverData, incomingData, sizeof(receiverData));
-  String inputData = "values " + String(receiverData.xAxisValue) + "  " + 
-                     String(receiverData.yAxisValue) + "  " + 
-                     String(receiverData.zAxisValue);
-  Serial.println(inputData);
+void OnDataRecv(uint8_t *mac_addr, uint8_t *incomingData, uint8_t len) {
+  if (len == 0) return;
+  
+  memcpy(&data, incomingData, sizeof(data));
+  lastRecvTime = millis();
 
-  // Process movement based on received values
-  if (receiverData.yAxisValue < 75)
-  {
-    processCarMovement(FORWARD);  
-  }
-  else if (receiverData.yAxisValue > 175)
-  {
-    processCarMovement(BACKWARD);     
-  }
-  else if (receiverData.xAxisValue > 175)
-  {
-    processCarMovement(RIGHT);   
-  }
-  else if (receiverData.xAxisValue < 75)
-  {
-    processCarMovement(LEFT);    
-  }
-  else
-  {
-    processCarMovement(STOP);     
-  }
+  // Print raw accelerometer values
+  Serial.print("Accelerometer Data - X: ");
+  Serial.print(data.xAxis);
+  Serial.print(" Y: ");
+  Serial.print(data.yAxis);
+  Serial.print(" Z: ");
+  Serial.println(data.zAxis);
 
-  lastRecvTime = millis();   
+  // Forward
+  if (data.yAxis < 75) {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    Serial.println("Action: Moving Forward");
+  }
+  // Backward
+  else if (data.yAxis > 175) {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    Serial.println("Action: Moving Backward");
+  }
+  // Right
+  else if (data.xAxis > 175) {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    Serial.println("Action: Turning Right");
+  }
+  // Left
+  else if (data.xAxis < 75) {
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    Serial.println("Action: Turning Left");
+  }
+  // Stop
+  else {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+    Serial.println("Action: Stopped");
+  }
+  Serial.println("------------------------");
 }
 
-void processCarMovement(int inputValue)
-{
-  switch(inputValue)
-  {
-    case FORWARD:
-      rotateMotor(RIGHT_MOTOR, MAX_MOTOR_SPEED);
-      rotateMotor(LEFT_MOTOR, MAX_MOTOR_SPEED);                  
-      break;
-  
-    case BACKWARD:
-      rotateMotor(RIGHT_MOTOR, -MAX_MOTOR_SPEED);
-      rotateMotor(LEFT_MOTOR, -MAX_MOTOR_SPEED);   
-      break;
-  
-    case LEFT:
-      rotateMotor(RIGHT_MOTOR, MAX_MOTOR_SPEED);
-      rotateMotor(LEFT_MOTOR, -MAX_MOTOR_SPEED);   
-      break;
-  
-    case RIGHT:
-      rotateMotor(RIGHT_MOTOR, -MAX_MOTOR_SPEED);
-      rotateMotor(LEFT_MOTOR, MAX_MOTOR_SPEED);  
-      break;
-  
-    case STOP:
-    default:
-      rotateMotor(RIGHT_MOTOR, STOP);
-      rotateMotor(LEFT_MOTOR, STOP);    
-      break;
-  }
-}
-
-void rotateMotor(int motorNumber, int motorSpeed)
-{
-  if (motorSpeed < 0)
-  {
-    digitalWrite(motorPins[motorNumber].pinIN1, LOW);
-    digitalWrite(motorPins[motorNumber].pinIN2, HIGH);    
-  }
-  else if (motorSpeed > 0)
-  {
-    digitalWrite(motorPins[motorNumber].pinIN1, HIGH);
-    digitalWrite(motorPins[motorNumber].pinIN2, LOW);       
-  }
-  else
-  {
-    digitalWrite(motorPins[motorNumber].pinIN1, LOW);
-    digitalWrite(motorPins[motorNumber].pinIN2, LOW);      
-  }
-  
-  analogWrite(motorPins[motorNumber].pinEn, abs(motorSpeed));
-}
-
-void setUpPinModes()
-{
-  for (int i = 0; i < motorPins.size(); i++)
-  {
-    pinMode(motorPins[i].pinIN1, OUTPUT);
-    pinMode(motorPins[i].pinIN2, OUTPUT);
-    pinMode(motorPins[i].pinEn, OUTPUT);
-    rotateMotor(i, STOP);  
-  }
-}
-
-void setup() 
-{
-  setUpPinModes();
-  
+void setup() {
   Serial.begin(115200);
+  
+  // Set motor pins as outputs
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  
+  // Stop motors initially
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+  
+  // Setup ESP-NOW
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
-
-  // Init ESP-NOW
-  if (esp_now_init() != 0) 
-  {
-    Serial.println("Error initializing ESP-NOW");
+  
+  if (esp_now_init() != 0) {
+    Serial.println("ESP-NOW failed to init");
     return;
   }
-
-  // Register callback function
+  
   esp_now_register_recv_cb(OnDataRecv);
+  Serial.println("Ready to receive accelerometer data");
+  Serial.println("Movement triggers:");
+  Serial.println("Forward: Y < 75");
+  Serial.println("Backward: Y > 175");
+  Serial.println("Right: X > 175");
+  Serial.println("Left: X < 75");
+  Serial.println("------------------------");
 }
- 
-void loop() 
-{
-  // Check for signal timeout
-  unsigned long now = millis();
-  if (now - lastRecvTime > SIGNAL_TIMEOUT) 
-  {
-    processCarMovement(STOP); 
+
+void loop() {
+  // Stop motors if no signal received for 1 second
+  if (millis() - lastRecvTime > TIMEOUT) {
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, LOW);
+    Serial.println("Timeout - Motors Stopped");
   }
 }
